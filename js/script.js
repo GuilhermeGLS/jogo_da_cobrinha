40afa82c669d3c4f6d1739654ab9bcfac7e9ad93
Guilherme Lima e Silva <guilhermelimaesilva.p@gmail.com>
2024-04-17 15:32:41 -0300

    add: Functions para a criação do grid e comida ao iniciar o jogo

--- js/script.js
+++ js/script.js
@@ -3,13 +3,31 @@ const ctx = canvas.getContext("2d")
 
 const size = 30
 
-const snake = [
-    { x: 200, y: 200 },
-    { x: 230, y: 200 }
+const snake = [ { x: 270, y: 240 }]
 
-]
+const randomNumber = () => {
+    
+}
+
+const food = {
+    x: 90,
+    y: 90,
+    color: "red"
+}
 
 let direction, loopId
+
+// Desenhando a comida
+const drawFood = () => {
+
+    const { x, y, color } = food
+
+    ctx.shadowColor = color
+    ctx.shadowBlur = 12
+    ctx.fillStyle = color
+    ctx.fillRect(x,y, size, size)
+    ctx.shadowBlur = 0
+}
 
 // Desenho da cobra
 const drawSnake = () => {
@@ -52,17 +70,40 @@ const moveSnake = () => {
     snake.shift()
 }
 
+// Desenhando Grid
+const drawGrid = () => {
+    ctx.lineWidth = 1
+    ctx.strokeStyle = "#191919"
+
+// Loop para desenhar todas de uma vez
+    for (let i = 30; i < canvas.width; i += 30) {
+        ctx.beginPath()
+        ctx.lineTo(i, 0);
+        ctx.lineTo(i, 600);
+        ctx.stroke()
+     
+        ctx.beginPath()
+        ctx.lineTo(0, i);
+        ctx.lineTo(600, i);
+        ctx.stroke()
+
+    }
+
+
+}
+
 const gameLoop = () => {
     clearInterval(loopId)
 
     ctx.clearRect(0, 0, 600, 600)
-
+    drawGrid()
+    drawFood()
     moveSnake()
     drawSnake()
 
     loopId = setTimeout(() => {
         gameLoop()
-    }, 300)
+    }, 250)
 }
 
 gameLoop()
@@ -77,7 +118,7 @@ document.addEventListener("keydown", ({ key }) => {
     if (key === "ArrowLeft" && direction !== "right") {
         direction = "left"
     }
-    
+
     if (key === "ArrowDown" && direction !== "up") {
         direction = "down"
     }

93c9a3d0afb4b00485167fb4b570fe71c38124c0
Guilherme Lima e Silva <guilhermelimaesilva.p@gmail.com>
2024-04-17 16:05:11 -0300

    add: Função que checa se a cobrinha comeu a comida

--- js/script.js
+++ js/script.js
@@ -3,16 +3,30 @@ const ctx = canvas.getContext("2d")
 
 const size = 30
 
-const snake = [ { x: 270, y: 240 }]
+const snake = [{ x: 270, y: 240 }]
 
-const randomNumber = () => {
-    
+// Função para spawn aleatorio da comida
+const randomNumber = (min, max) => {
+    return Math.round(Math.random() * (max - min) + min)
+}
+
+const randomPosition = () => {
+    const number = randomNumber(0, canvas.width - size)
+    return Math.round(number / 30) * 30
+}
+
+const randomColor = () => {
+    const red = randomNumber(0, 255)
+    const green = randomNumber(0, 255)
+    const blue = randomNumber(0, 255)
+
+    return `rgb(${red}, ${green}, ${blue})`
 }
 
 const food = {
-    x: 90,
-    y: 90,
-    color: "red"
+    x: randomPosition(),
+    y: randomPosition(),
+    color: randomColor()
 }
 
 let direction, loopId
@@ -25,7 +39,7 @@ const drawFood = () => {
     ctx.shadowColor = color
     ctx.shadowBlur = 12
     ctx.fillStyle = color
-    ctx.fillRect(x,y, size, size)
+    ctx.fillRect(x, y, size, size)
     ctx.shadowBlur = 0
 }
 
@@ -75,13 +89,13 @@ const drawGrid = () => {
     ctx.lineWidth = 1
     ctx.strokeStyle = "#191919"
 
-// Loop para desenhar todas de uma vez
+    // Loop para desenhar todas de uma vez
     for (let i = 30; i < canvas.width; i += 30) {
         ctx.beginPath()
         ctx.lineTo(i, 0);
         ctx.lineTo(i, 600);
         ctx.stroke()
-     
+
         ctx.beginPath()
         ctx.lineTo(0, i);
         ctx.lineTo(600, i);
@@ -92,18 +106,32 @@ const drawGrid = () => {
 
 }
 
+// Checando se a cobra comeu a comida
+const checkEat = () => {
+    const head = snake[snake.length - 1]
+
+    if (head.x == food.x && head.y == food.y) {
+        snake.push(head)
+
+        food.x = randomPosition(),
+        food.y = randomPosition(),
+        food.color = randomColor()
+    }
+}
+
 const gameLoop = () => {
     clearInterval(loopId)
 
     ctx.clearRect(0, 0, 600, 600)
     drawGrid()
     drawFood()
+    checkEat()
     moveSnake()
     drawSnake()
 
     loopId = setTimeout(() => {
         gameLoop()
-    }, 250)
+    }, 80)
 }
 
 gameLoop()
